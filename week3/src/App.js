5c6edff951697be6abfa670159b127cf5600a4d7
newtronahmed <bazubairu@st.ug.edu.gh>
2021-06-18 04:32:58 +0000

    week 2 and 3

---
+++ week3/src/App.js
@@ -0,0 +1,66 @@
+import React,{useState} from 'react';
+import "./styles.css";
+
+export default function App() {
+  const [isLightTheme, setTheme] = useState(true)
+  const toggleTheme = ()=>{
+    setTheme(!isLightTheme)
+  }
+  return (
+    <div className="main">
+    <div className="container">
+      <h1 className={`${isLightTheme ? 'light-primary' : 'dark-primary'}`}>UNDER CONSTRUCTION</h1>
+      <h4  className={`${isLightTheme ? 'light-secondary' : 'dark-secondary'}`}>
+        We have a few quirks to fix , but we will have everything up and running
+        in no time . Please comeback in a few hours
+      </h4>
+      <button onClick={()=>toggleTheme()}>{`${isLightTheme ? 'dark theme' : 'light theme'}`}</button>
+      <div className="box-container">
+        <div className={`box ${isLightTheme ? 'light-box' : 'dark-box'}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-6 w-6 ${isLightTheme ? 'light-primary' : 'light-secondary'} `}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>
+
+          <h2>Contact Us</h2>
+          <p>For any question please send us a message to hello@gmail.com</p>
+        </div>
+        <div className={`box ${isLightTheme ? 'light-box' : 'dark-box'}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-5 w-5 ${isLightTheme ? 'light-primary' : 'light-secondary'} `}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+          </svg>
+          <h2>Support Forum</h2>
+          <p>You are invited to visit our support forum and knowledge base</p>
+        </div>
+        <div className={`box ${isLightTheme ? 'light-box' : 'dark-box'}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={`h-5 w-5 ${isLightTheme ? 'light-primary' : 'light-secondary'} `}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+          </svg>
+          <h2 >Customer Service</h2>
+          <p>To talk to a representative please call 190-9765-3544</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
